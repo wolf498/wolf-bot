d21f63a06b18981a9166afccd95dc2ce3566327e
wolf498 <81432558+wolf498@users.noreply.github.com>
2022-12-02 18:35:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,46 +1,45 @@
 module.exports = {
-	cmdPerPage: 10, //- Number of commands per page of help command
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
-	port: 4200, //- Port of the API and Dashboard
-	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
-	serverDeafen: true, //- If you want bot to stay deafened
-	defaultVolume: 100, //- Sets the default volume of the bot, You can change this number anywhere from 1 to 100
-	supportServer: "https://discord.gg/sbySMS7m3v", //- Support Server Link
-	Issues: "https://github.com/SudhanPlayz/Discord-MusicBot/issues", //- Bug Report Link
-	permissions: 277083450689, //- Bot Inviting Permissions
-	disconnectTime: 30000, //- How long should the bot wait before disconnecting from the voice channel (in miliseconds). Set to 1 for instant disconnect.
-	twentyFourSeven: false, //- When set to true, the bot will never disconnect from the voice channel
-	autoQueue: false, //- When set to true, related songs will automatically be added to the queue
-	autoPause: true, //- When set to true, music will automatically be paused if everyone leaves the voice channel
-	debug: false, //- Debug mode
-	cookieSecret: "CodingWithSudhan is epic", //- Cookie Secret
-	website: "http://localhost:4200", //- without the / at the end
-	// You need a lavalink server for this bot to work!!!!
-	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
-	nodes: [
-		{
-			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
-			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
-			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
-			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
-		},
-	],
-	embedColor: "#2f3136", //- Color of the embeds, hex supported
-	presence: {
-		// PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-		status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
-		activities: [
-			{
-				name: "Music", //- Status Text
-				type: "LISTENING", //- PLAYING, WATCHING, LISTENING, STREAMING
-			},
-		],
-	},
-	iconURL: "https://cdn.darrennathanael.com/icons/spinning_disk.gif", //- This icon will be in every embed's author field
+  Admins: ["866383640648351754", "796528269716881439"], // Admins of the bot, I don't know what this do. -Darren.
+  ExpressServer: true, // If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "WS", // Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
+  Token: process.env.Token || "OTIzMzI3MDIxOTIzMzE1NzMy.GOzn7a.v6CPoXuNSErXDUScGn2hmPLDZJvoxzOfGrlt-I", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "923327021923315732", // Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "ArYqz4RQeeq1QNEaFfX-I5E7QQimGPuP", // Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
+  ServerDeafen: true, // If you want bot to stay deafened
+  DefaultVolume: 3, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
+  CallbackURL: "/api/callback", // Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
+  "24/7": true, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
+  CookieSecret: "Pikachu is cute", // A cookie for you, cookie for me. make sure you change this value!
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  EmbedColor: "RANDOM", // Color of most embeds | Custom Hex value are supported. I.e: "#36393F"
+  Permissions: 2205281600, // Bot Inviting Permissions
+  Website: process.env.Website || "https://example.com", // Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  // If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
+
+  Presence: {
+    status: "online", // You can show online, idle, and dnd
+    name: "Music", // The message shown
+    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+  },
+
+  // You need a lavalink server for this bot to work!!!!
+  // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
+  Lavalink: {
+    id: "Main", //- Used for indentifier. You can set this to whatever you want.
+    host: "lavalink-replit-2.onewolfwolf.repl.co", //- The host name or IP of the lavalink server.
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "maybeiwasboring", //- The password of the lavalink server.
+    secure: true, // Set this to true if the lavalink uses SSL. if not set it to false.
+    retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
+    retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
+  },
+  // Spotify Integration, allows you to enter a spotify link.
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "", // Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "", // Spotify Client Secret
+  },
 };
